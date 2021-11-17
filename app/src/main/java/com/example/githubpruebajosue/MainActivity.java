6af7983e38e88ea7b8c71505e217eb90c9695f02
JosueJurgen <015200256c@uandina.edu.pe>
2021-11-16 19:46:48 -0500

    Initial commit

---
+++ app/src/main/java/com/example/githubpruebajosue/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.githubpruebajosue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
